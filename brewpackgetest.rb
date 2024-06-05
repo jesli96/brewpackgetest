62824366cf8374dbb5a065e6bc742bdd735c398a
Jesli Albert Bautista <japbautista96@gmail.com>
2024-06-06 04:08:06 +0800

    added forumla

---
+++ brewpackgetest.rb
@@ -0,0 +1,21 @@
+class brewpackgetest < Formula
+  desc "A Spring Boot application"
+  homepage "https://github.com/jesli96/brewpackgetest"
+  url "https://github.com/jesli96/brewpackgetest/releases/download/1.0/updatedep-0.0.1-SNAPSHOT.jar"
+  version "1.0"
+  sha256 "6344A98F3BA9C728C4114C9249F1B9DB210851B646D36A9F9A418780B3526193"
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "updatedep-0.0.1-SNAPSHOT.jar"
+    (bin/"brewpackgetest").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/updatedep-0.0.1-SNAPSHOT.jar" "$@"
+    EOS
+  end
+
+  test do
+    system "#{bin}/brewpackgetest", "--version"
+  end
+end
