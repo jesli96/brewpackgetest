9fca1f0476077cd9254d0f3fcde85e019d2253c8
jesli96 <108883028+jesli96@users.noreply.github.com>
2024-06-08 01:34:08 +0800

    Update Brewpackgetest.rb

--- Formula/Brewpackgetest.rb
+++ Formula/Brewpackgetest.rb
@@ -2,8 +2,7 @@ class Brewpackgetest < Formula
   desc "A Spring Boot application"
   homepage "https://github.com/jesli96/homebrew-brewpackgetest"
   url "https://github.com/jesli96/homebrew-brewpackgetest/releases/download/1.0/updatedep-0.0.1-SNAPSHOT.jar"
-  # version "1.0"
-  sha256 "6344A98F3BA9C728C4114C9249F1B9DB210851B646D36A9F9A418780B3526193"
+  sha256 “6344a98f3ba9c728c4114c9249f1b9db210851b646d36a9f9a418780b3526193”
   license "MIT"
 
   depends_on "openjdk"
@@ -11,11 +10,6 @@ class Brewpackgetest < Formula
   def install
     libexec.install "updatedep-0.0.1-SNAPSHOT.jar"
     bin.write_jar_script libexec/"updatedep-0.0.1-SNAPSHOT.jar", "brewpackgetest"
-    # libexec.install "updatedep-0.0.1-SNAPSHOT.jar"
-    # (bin/"homebrew-brewpackgetest").write <<~EOS
-    #   #!/bin/bash
-    #   exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/updatedep-0.0.1-SNAPSHOT.jar" "$@"
-    EOS
   end
 
   test do
