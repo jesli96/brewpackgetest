c054069f45c08004ec8f86aa2f499eb592e52ce5
jesli96 <108883028+jesli96@users.noreply.github.com>
2024-06-08 01:45:35 +0800

    Update Brewpackgetest.rb

--- Formula/Brewpackgetest.rb
+++ Formula/Brewpackgetest.rb
@@ -2,7 +2,7 @@ class Brewpackgetest < Formula
   desc "A Spring Boot application"
   homepage "https://github.com/jesli96/homebrew-brewpackgetest"
   url "https://github.com/jesli96/homebrew-brewpackgetest/releases/download/1.0/updatedep-0.0.1-SNAPSHOT.jar"
-  sha256 “6344a98f3ba9c728c4114c9249f1b9db210851b646d36a9f9a418780b3526193”
+  sha256 "6344a98f3ba9c728c4114c9249f1b9db210851b646d36a9f9a418780b3526193"
   license "MIT"
 
   depends_on "openjdk"
